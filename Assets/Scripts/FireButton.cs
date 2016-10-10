a8ad07e339289a2926b07040726548b2022ecc7f
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-10-09 21:07:03 -0500

    agregado un comentario

--- Assets/Scripts/FireButton.cs
+++ Assets/Scripts/FireButton.cs
@@ -18,3 +18,5 @@ public class FireButton : MonoBehaviour, IVirtualButtonEventHandler {
 }
 //este script deberia tener la opcion de deshabilitar o habilitar el boton, si es que estoy en una zona en la que puedo lanzar
 //funciona pero hay que corregir los otrs scripts
+//este script debe tener un booleano que para qeu puesa ser consultado desde afuera, o podria modificar un booleano externo en cada cambio, para asi poder hacer lo mismo que haci antes para llenar la barra de fuerza
+//evaluar las idstintas tecnicas
